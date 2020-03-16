4deec0dd13bffa5992198e0ca93b2cdd2c1881b2
Rhett Rogers <rhettmrogers@icloud.com>
2020-03-16 11:40:23 -0600

    Initial Commit

---
+++ Cardshark/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Cardshark
+//
+//  Created by Rhett Rogers on 3/16/20.
+//  Copyright © 2020 LyokoTech. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
